d7f78a69afc875273eed3e6a30dc995000bd3444
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2022-01-16 20:11:43 -0600

    models added

---
+++ server/models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+ name: {
+    type: String,
+    required: true,
+    },
+    price: {
+        type: Number, 
+        required: true, 
+        min: 0.99
+    },
+    image: {
+        type: String, 
+        
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: 0
+    }
+    ,
+    description: {
+        type: String
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.export = Product;
---
+++ server/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+    email: {
+        type: Number,
+        required: true,
+  },
+  phoneNumber: {
+    type: Number,
+  },
+  address: {
+      type: String,
+      required:true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.export = User;
