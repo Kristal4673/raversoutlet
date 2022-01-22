593bbd94485936164963334b9588606131ff146a
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-22 11:33:43 -0600

    Added Auth  and actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,4 +1,6 @@
-export const ADD_CAR = 'ADD_CAR';
-export const REMOVE_CAR = 'REMOVE_CAR';
-export const START_CAR = 'START_CAR';
-export const STOP_CAR = 'STOP_CAR';
+export const ADD_TO_CART = "ADD_TO_CART";
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CLEAR_CART = "CLEAR_CART";
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+export const TOGGLE_CART = "TOGGLE_CART";
