c0a93ebea20f733b0e5b7c3423dd057ec93a5b38
Gustavo Garcia <gustavogarcia42@yahoo.com>
2022-01-22 15:21:59 -0600

    Started categoryseed json and added password to model with hashing

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 
 module.exports = Category;
