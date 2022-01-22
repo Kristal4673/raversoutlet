07a189bd2989fc850c36ff922fc1024bec5f7fce
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2022-01-22 13:08:16 -0600

    routes and style pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,30 @@ import Navbar from './Components/Navbar/Navbar';
 import Main from './pages/Main/Main';
 import { ApolloClient } from '@apollo/client';
 import Footer from './Components/Footer/Footer';
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Signin from "./pages/Signin";
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Shop from './pages/Shop';
-import SignUp from './pages/SignUp';
+import Cart from "./pages/Cart/Cart";
+import Checkout from "./pages/Checkout/Checkout";
+import Login from "./pages/Login/Login";
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import SignUp from './pages/SignUp/SignUp';
+// import Profile from './pages/Profile/Profile';
+import { Profiler } from 'react';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Main />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/shop/category/:name" element={<Shop />} />
+          <Route path="/profile" element={<Profiler />} />
+          <Route path="/main/category/:name" element={<Main/>} />
           <Route path="/product/:id" element={<Product />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
       <Footer />
--- client/src/pages/SignUp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SignUp = () => {
-  return <div> Sign up link </div>;
-};
-
-export default SignUp;
