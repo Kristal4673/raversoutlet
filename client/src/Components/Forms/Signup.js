dbc87410a3871a1d103bb6b3396a891a7486040c
bperez05 <perez.brandon55@yahoo.com>
2022-01-29 15:08:28 -0600

    mergered from main

--- client/src/Components/Forms/Signup.js
+++ client/src/Components/Forms/Signup.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import HomeImg from '../../img/home.png';
+import './Form.css'
 
-function Signup() {
-    return (
-        <div>
-            
+
+            <div className='form'>
+            <div className='form-ctn'>
+                <div className='form-title'>Login</div>
+           <img className='ravers-logo' alt='raversoutlet' src={HomeImg}/> 
         </div>
-    )
-}
+  
 
-export default Signup
+
